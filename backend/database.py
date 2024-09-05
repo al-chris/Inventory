acf4292085379ce3705377e00652a07bf76b0a26
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-09-05 20:23:09 +0100

    connected to external db

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,17 @@
 # backend/database.py
+import os
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./inventory.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# DATABASE_URL = "sqlite:///./inventory.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
